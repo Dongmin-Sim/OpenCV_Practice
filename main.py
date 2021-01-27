68c6827625be476d5435726ca20cc301ed688039
codongmin <codongmin@gmail.com>
2021-01-27 13:35:23 +0900

    이미지를 grayscale 로 불러오고 저장하는 코드 추가, grayscale 사진 저장

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 
-img_file = 'img/sample.png'
+img_file = 'img/man1.jpg'
 img = cv2.imread(img_file)
 
 if img is not None:
@@ -9,3 +9,16 @@ if img is not None:
     cv2.destroyAllWindows()
 else:
     print('NO IMAGE FILE')
+
+
+# 그레이 색상으로 불러온 후 저장하는 법
+save_file = 'img/man1_gray.jpg'
+
+img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)  # 컬러옵션, cv2.IMREAD_GRAYSCALE 그레이 색상으로 불러오는 인자값
+cv2.imshow(img_file, img)
+
+# 파일로 저장, 포맷을 확장자에 따름
+cv2.imwrite(save_file, img)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
